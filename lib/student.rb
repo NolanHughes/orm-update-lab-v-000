d7f730ee1ac0c0e1f8b78eafeef7d5287eba056f
Nolan Hughes <nhughes987@gmail.com>
2018-01-10 02:40:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,13 +52,13 @@ class Student
   end
 
   def self.new_from_db(row)
-    binding.pry
+    # binding.pry
     student = Student.create(row[1], row[2])
-    binding.pry
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    # binding.pry
+    # student.id = row[0]
+    # student.name = row[1]
+    # student.grade = row[2]
+    # student
   end
 
 end
