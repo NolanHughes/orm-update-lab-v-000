94ce235b9aef771eeeaa1e5037c2b93abb78c7f1
Nolan Hughes <nhughes987@gmail.com>
2018-01-10 02:38:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
 
   def self.new_from_db(row)
     binding.pry
-    student = Student.save
+    student = self.save
     binding.pry
     student.id = row[0]
     student.name = row[1]
