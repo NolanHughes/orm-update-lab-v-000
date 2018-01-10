91f48db10c25dae1eb3261f98e3f9b205b428ed5
Nolan Hughes <nhughes987@gmail.com>
2018-01-10 03:11:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = Student.new(row[1], row[2])
+    student = Student.new(row[0], row[1], row[2])
     student.save
     student
   end
